a622d10b5fad29af0e00926214bbd9ff1d771eb2
tak_sas <takuminakayama4869@gmail.com>
2022-08-09 23:20:01 +0900

    local storage => code

--- popup.js
+++ popup.js
@@ -1,15 +1,16 @@
 // 拡張機能がインストールされたときの処理
 chrome.runtime.onInstalled.addListener(function(){
+    chrome.storage.sync.set({'code1': "未"}, function(){});
+    chrome.storage.sync.set({'code2': "取得"}, function(){});
   });
 
 
 
-chrome.storage.sync.get(['value'], function (value){document.getElementById("counter").value = JSON.stringify(value.value).replace('"', '').replace('"', '');});
+chrome.storage.sync.get(['code1'], function (value){document.getElementById("code1").textContent = JSON.stringify(value.code1).replace('"', '').replace('"', '');});
+chrome.storage.sync.get(['code2'], function (value){document.getElementById("code2").textContent = JSON.stringify(value.code2).replace('"', '').replace('"', '');});
 
 
 document.getElementById("btnnew").addEventListener("click", async () => {
-    document.getElementById("code1").textContent = "123";
-    document.getElementById("code2").textContent = "456";
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
@@ -24,7 +25,19 @@ document.getElementById("btnnew").addEventListener("click", async () => {
   });
 
 
-  
+
+  document.getElementById("codeok").addEventListener("click", async () => {
+    var code1 = document.getElementById("codeform1").value;
+    var code2 = document.getElementById("codeform2").value;
+    document.getElementById("code1").textContent = code1;
+    document.getElementById("code2").textContent = code2;
+    chrome.storage.sync.set({'code1': code1}, function(){});
+    chrome.storage.sync.set({'code2': code2}, function(){});
+    document.getElementById("entercode").style.display = 'none';
+  });
+
+
+
   document.getElementById("copybtn").addEventListener("click", async () => {
     // コピー対象をJavaScript上で変数として定義する
     var copyTarget = document.getElementById("txt");

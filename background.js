c9f5c71c31379d161e2fe04f82c011bd3793cdb0
tak_sas <takuminakayama4869@gmail.com>
2022-08-10 14:24:19 +0900

    context menu

--- background.js
+++ background.js
@@ -1,5 +1,49 @@
-var test = "1000";
-chrome.storage.sync.set({'value': test}, function(){});
+// 拡張機能がインストールされたときの処理
+chrome.runtime.onInstalled.addListener(function(){
+    chrome.storage.sync.set({'code1': "未"}, function(){});
+    chrome.storage.sync.set({'code2': "取得"}, function(){});
+
+
+
+    //親メニューの追加
+  const parent = chrome.contextMenus.create({
+    type: "normal",
+    id: "parent",
+    title: "SlickClickPasteでクラウドデータと同期"
+  });
+
+  //子メニューを追加（１つ目）
+  const child1 = chrome.contextMenus.create({
+    type: "normal",
+    id: "child1",
+    parentId: "parent",
+    title: "クリップボードのデータをクラウドへアップロード"
+  });
+  //子メニューを追加（２つ目）
+  const child2 = chrome.contextMenus.create({
+    type: "normal",
+    id: "child2",
+    parentId: "parent",
+    title: "クラウドのデータをクリップボードにダウンロード"
+  });
+
+  
+  });
+
+
+
+
+
+
+
+sync();
+
+
+
+
+
+
+
 
 
 chrome.action.onClicked.addListener((tab) => {
@@ -10,8 +54,79 @@ chrome.action.onClicked.addListener((tab) => {
   });
 
 
+async function sync() {
+
+    chrome.storage.sync.get(['code1'], function (value){
+        var code1 = JSON.stringify(value.code1).replace('"', '').replace('"', '');
+        chrome.storage.sync.get(['code2'], function (value){
+            var code2 = JSON.stringify(value.code2).replace('"', '').replace('"', '');
 
 
+            // sign_in
+            const data = 
+            {
+            "email": "easy_access@" + code1 + code2 +".com",
+            "password": "password"
+            };
+
+            fetch('http://127.0.0.1:3000/v1/auth/sign_in', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            })
+            .then(response => {
+            if(response.status!==200)
+            {
+                throw new Error(response.status)
+            }
+                var access_token = response.headers.get('access-token');
+                var client = response.headers.get('client');
+                var expiry = response.headers.get('expiry');
 
 
+                fetch('http://127.0.0.1:3000/v1/users/get', {
+                            method: 'GET',
+                            headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            'access-token': access_token,
+                            'uid': "easy_access@" + code1 + code2 +".com",
+                            'client': client,
+                            'expiry': expiry,
+                            'token-type': 'Bearer',
+                            }
+                            })
+                            .then(response => {
+                                response.json().then(data => {
+                                    var clipboard = data.clipboard.text;
+                                    chrome.storage.sync.set({'clipboard': clipboard}, function(){});
+                                })
+                                chrome.storage.sync.set({'status': "OK"}, function(){});
+                            })
+                            .then(data => {
+                                console.log('Success:', data);
+                            })
+                            
+                            .catch((error) => {
+                            
+                            });
 
+
+            })
+            .then(data => {
+            console.log('Success:', data);
+            })
+            .catch((error) => {
+            chrome.storage.sync.set({'status': "NG"}, function(){});
+            });
+            
+            
+
+        });
+    });
+    const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    await _sleep(7000);
+    sync();
+}
